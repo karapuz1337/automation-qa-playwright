959eed4e7cf1702edba341b58b9b9795999a8686
karapuz1337 <belanoff228@gmail.com>
2025-11-29 22:31:22 +0200

    feat: polish config.js and playwright.config.js

--- config/config.js
+++ config/config.js
@@ -1,11 +1,14 @@
 import "dotenv/config";
 
+const hasHttpCreds = process.env.HTTP_USERNAME && process.env.HTTP_PASSWORD;
+
 const config = {
   baseUrl: process.env.BASE_URL,
-  httpCredentials: {
+  httpCredentials: hasHttpCreds ? {
     username: process.env.HTTP_USERNAME,
     password: process.env.HTTP_PASSWORD
   }
+    : undefined
 };
 
 export default config;
--- playwright.config.js
+++ playwright.config.js
@@ -3,14 +3,6 @@ import { defineConfig, devices } from "@playwright/test";
 import config from "./config/config.js";
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
