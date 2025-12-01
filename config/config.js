08dcc7221815ece91930aa1ac3686ccc8debcef7
karapuz1337 <belanoff228@gmail.com>
2025-12-01 11:41:13 +0200

    fix: remove the hasHttpCreds constant from config.js

--- config/config.js
+++ config/config.js
@@ -1,14 +1,11 @@
 import "dotenv/config";
 
-const hasHttpCreds = process.env.HTTP_USERNAME && process.env.HTTP_PASSWORD;
-
 const config = {
   baseUrl: process.env.BASE_URL,
-  httpCredentials: hasHttpCreds ? {
+  httpCredentials: {
     username: process.env.HTTP_USERNAME,
     password: process.env.HTTP_PASSWORD
   }
-    : undefined
 };
 
 export default config;
