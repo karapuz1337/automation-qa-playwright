831ebb4fa7737fbf21f0ad8d98fcc176e6e86155
karapuz1337 <belanoff228@gmail.com>
2025-12-13 23:04:18 +0200

    fix: add leading slash to url at ProfilePage.js constructor

--- src/pageObjects/panel/profile/ProfilePage.js
+++ src/pageObjects/panel/profile/ProfilePage.js
@@ -2,7 +2,7 @@ import BasePage from "../../BasePage.js";
 
 export default class ProfilePage extends BasePage {
   constructor(page) {
-    super(page, "panel/profile");
+    super(page, "/panel/profile");
     this.profileName = page.locator(".profile_name");
   }
 
